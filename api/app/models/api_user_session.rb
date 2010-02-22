4bf0d0a6966d41cb3a0f05ee5939ad9a8d8ee2ad
Colin Young <me@colinyoung.name>
2010-02-21 22:11:41 -0600

    updating setting user active

--- api/app/models/api_user_session.rb
+++ api/app/models/api_user_session.rb
@@ -6,6 +6,6 @@ class ApiUserSession < Authlogic::Session::Base
   def check_if_verified
     errors.add(:base, "Your account has not been verified.  
     Please check your spam folder at the email address 
-    you used to register.") unless attempted_record && attempted_record.active
+    you used to register.") unless attempted_record && (attempted_record.active == true)
   end
 end
