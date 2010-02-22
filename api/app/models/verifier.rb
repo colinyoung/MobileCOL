971cdff3a81fede448e58707be0a682125d7561b
Colin Young <me@colinyoung.name>
2010-02-21 20:51:53 -0600

    stopped cc'ing on emails, they're working now

--- api/app/models/verifier.rb
+++ api/app/models/verifier.rb
@@ -1,7 +1,6 @@
 class Verifier < ActionMailer::Base
   def signup_notification(user)
     recipients  user.email
-    cc           "me@colinyoung.name"
     from         "accounts@#{PRODUCT_DOMAIN}"
     subject       "Your API account on #{PRODUCT_NAME}"
     body          :account => user
